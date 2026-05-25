5c16007bb68bd46eddf14953c1e6b1fef9ed03a1
Dev PLAI <dev@plai.ac.id>
2026-05-25 13:32:12 +0700

    feat(branding): update favicon, logo assets, and app title to SPMI

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,14 +5,14 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <link rel="icon" href="/favicon.ico" sizes="any">
-        <link rel="icon" href="/favicon.svg" type="image/svg+xml">
+        <link rel="icon" href="/favicon.ico" type="image/ico+xml">
         <link rel="apple-touch-icon" href="/apple-touch-icon.png">
 
         @fonts
 
         @vite(['resources/css/app.css', 'resources/js/app.ts'])
         <x-inertia::head>
-            <title>{{ config('app.name', 'Laravel') }}</title>
+            <title>{{ config('app.name', 'SPMI') }}</title>
         </x-inertia::head>
     </head>
     <body class="font-sans antialiased">
