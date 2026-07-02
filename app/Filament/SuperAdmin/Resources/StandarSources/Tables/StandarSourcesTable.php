46c270b96d97521ad8d9c52618d43ec2935d4d83
unknown <muhammadfebby212@gmail.com>
2026-07-02 20:59:33 +0700

    chore: remove pail from dev script and add searchable to standard sources table

--- app/Filament/SuperAdmin/Resources/StandarSources/Tables/StandarSourcesTable.php
+++ app/Filament/SuperAdmin/Resources/StandarSources/Tables/StandarSourcesTable.php
@@ -15,13 +15,13 @@ class StandarSourcesTable
     {
         return $table
             ->columns([
-                TextColumn::make('name')->label('Nama'),
-                TextColumn::make('code')->label('Kode'),
+                TextColumn::make('name')->label('Nama')->searchable(),
+                TextColumn::make('code')->label('Kode')->searchable(),
                 TextColumn::make('is_active')
-                ->label('Status')
-                ->formatStateUsing(fn($state) => $state ? 'AKTIF' : 'TIDAK AKTIF')
-                ->badge()
-                ->color(fn($state) => $state ? 'success' : 'danger'),
+                    ->label('Status')
+                    ->formatStateUsing(fn ($state) => $state ? 'AKTIF' : 'TIDAK AKTIF')
+                    ->badge()
+                    ->color(fn ($state) => $state ? 'success' : 'danger'),
             ])
             ->filters([
                 //
